799b57754e518ec12da23dd34e14c56372a957c1
9at8 <aditya.1998thakral@gmail.com>
2016-01-04 21:24:37 +0530

    Update v0.0.2 I decided to make 2 files, one as a module and the other as an interactive version.

--- sqlm.py
+++ sqlm.py
@@ -1,20 +1,22 @@
-import sys
-from time import sleep
+"""
+This module provides MySQL functions.
+"""
 
-try:
-    import mysql.connector as cn
-    print 'MySQL connection successful!'
-except ImportError, e:
-    print 'Import failed:', e.message
-    print 'Trying to import \'MySQLdb\''
-    sleep(2)
-    try:
-        import MySQLdb as cn
-        print 'MySQL connection successful!'
-    except ImportError, f:
-        print 'Import failed:', f.message
-        print 'Aborting.'
-        for i in range(5, 0, -1):
-            print i
-            sleep(1)
-        sys.exit()
+import mysql.connector
+
+
+class Manager(object):
+    def __init__(self, user, password, database=None, host='127.0.0.1'):
+        self.cnx = mysql.connector.connect(user=user, password=password, database=database, host=host)
+        self.cursor = self.cnx.cursor()
+
+    def exe(self, query, display=0):
+        # Just a shortcut for execution
+        self.cursor.execute(query)
+        if display:
+            for this in self.cursor:
+                print this
+
+    def tablemaker(self, tablename, database):
+        self.exe('use ' + database + ';')
+        self.exe('')
